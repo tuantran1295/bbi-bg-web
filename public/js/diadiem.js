de4c2291fcda06d6abf3fe975a57fc0de1730dd2
Tuantq <tuantran1245@gmail.com>
2019-12-23 17:02:47 +0700

    fix some bug on carousel

--- public/js/diadiem.js
+++ public/js/diadiem.js
@@ -1,24 +1,28 @@
 window.onload = function () {
     var body = document.getElementsByTagName("body")[0];
     body.onscroll = function () {
-        body.classList.add('scroll');
         // scroll to top
         if($(window).scrollTop() === 0){
-            body.classList.toggle('scroll');
-            body.classList.toggle('endpage');
+            body.classList.remove('scroll');
+        } else {
+            body.classList.add('scroll');
         }
         // scroll to bottom
         if($(window).scrollTop() + $(window).height() == $(document).height()) {
             body.classList.add('endpage');
+        } else {
+            body.classList.remove('endpage');
         }
     }
 }
 
 $(document).ready(function(){
-    $('.list-ct').slick({
+    $('.home-slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000
     });
 });
 
